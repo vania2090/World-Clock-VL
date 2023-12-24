7084bd2991d2d9467947080833db5bcf7840a04e
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-23 23:30:33 -0500

    added IDs

--- SRC/index.js
+++ SRC/index.js
@@ -1,9 +1,19 @@
 function showSelectedCity(event) {
+  let parisTime = moment.tz("Europe/Paris").format("dddd, MMMM D, YYYY h:m A");
   if (event.target.value.length > 0) {
-    let cityTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:m A");
-    alert(`It is ${cityTime} in ${event.target.value}`);
+    if (event.target.value === "Paris") {
+      alert(`It is ${parisTime} in Paris`);
+    }
+  }
+  let tokyoTime = moment.tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
+  if (event.target.value === "Tokyo") {
+    alert(`It is ${tokyoTime} in Tokyo`);
+  }
+  let sydneyTime = moment
+    .tz("Australia/Sydney")
+    .format("dddd, MMMM D, YYYY h:m A");
+  if (event.target.value === "Sydney") {
+    alert(`It is ${sydneyTime} in Sydney`);
   }
 }
 let citiesSelect = document.querySelector("#cities");
